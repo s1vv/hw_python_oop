fcba71ae88629096dfdfd0f673e3802845520787
Slava Fumin <a0413@ya.ru>
2023-02-02 15:24:29 +0300

    first commit

--- homework.py
+++ homework.py
@@ -83,7 +83,8 @@ class SportsWalking(Training):
         return ((self.K_WALK * self.weight + ((self.get_mean_speed()
                 * self.KM_H_M_S) ** 2 / (self.height / self.MM_TO_M))
                 * self.K_WALK2 * self.weight)
-                * (self.duration * self.MIN_IN_HOUR))
+                * (self.duration * self.MIN_IN_HOUR)
+                )
 
 
 @dataclass
